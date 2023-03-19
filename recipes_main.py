215b82e66e381478e74b4e2219e07e50e1e9bd3e
scallypr <mets1005@yahoo.com>
2023-03-19 16:58:04 -0400

    3/19 4:57

--- recipes_main.py
+++ recipes_main.py
@@ -4,7 +4,7 @@ from fooddata_central import *
 def recipes_main():
     os.system("clear")
 
-    oatmeal = {'quick oats': 40,
+    food = {'quick oats': 40,
              'powdered peanut butter': 13,
              'vanilla casein protein powder': 15,
              'chia seeds': 10,
@@ -13,8 +13,26 @@ def recipes_main():
              'plain non-fat greek yogurt': 100,
              'unsweetened applesauce': 50,
              'unsweetened vanilla almond milk': 100}
+
+    # food = {'boneless skinless chicken breasts': 908/5,
+    #         'carrots': 250/5,
+    #         'celery': 200/5,
+    #         'garlic': 22/5,
+    #         'onion': 100/5,
+    #         'yukon gold potatoes': 750/5,
+    #         'peas': 170/5,
+    #         'milk': 60/5,
+    #         'unsalted butter': 28/5,
+    #         'cornstarch': 30/5,
+    #         'water': 30/5,
+    #         'extra virgin olive oil': 30/5,
+    #         'granulated chicken bouillion': 8/5,
+    #         'salt': 1,
+    #         'black pepper': 1}
+
+    # TODO: input amount of servings
     
-    fc_main(oatmeal)
+    fc_main(food)
 
 
 if __name__ == "__main__" :
